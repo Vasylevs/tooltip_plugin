b36b9c8cd8e17f56bc90e0963fca8940707981e7
Seliv <selivonenko007@gmail.com>
2018-03-09 00:26:25 +0200

    make plugin function

---
+++ tooltip/jquery.tooltip.js
@@ -0,0 +1,33 @@
+;(function ($) {
+    $.fn.tooltip = function () {
+        $('body').append('<div class="tooltip-block"></div>');
+        var title;
+        return this.each(function () {
+            var tag = $(this).get(0).nodeName;
+            if( tag == 'SPAN'){
+                $(this).css({
+                    'border-bottom': '1px dashed red',
+                    'background': 'yellow'
+                });
+            };
+           $(this).mouseenter(function () {
+               title = $.trim($(this).attr('title'));
+               $(this).attr('title','');
+               if(title == undefined || title == ''){
+                   title = $(this).text();
+               }
+           }).mousemove(function (e) {
+                var top = e.clientY + 15;
+                var left = e.clientX + 15;
+                $('.tooltip-block').css({
+                    'left': left,
+                    'top': top
+                }).text(title);
+               $('.tooltip-block').show();
+           }).mouseleave(function () {
+               $('.tooltip-block').hide();
+               $(this).attr('title',title);
+           });
+        });
+    };
+})(jQuery);
